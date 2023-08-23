e837df69a6f974f07366561344760a6d1bd8cd3a
Erick Kamanda <erickckamanda@gmail.com>
2023-08-23 14:07:35 -0700

    Created the main function/entry point for the program

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * main - this is the entry point of the program
+ * @ac: the number of commandline arguments
+ * @argv: a pointer the commandline arguments
+ * Return: returns 0 on success
+ */
+int main(int ac, char *argv[])
+{
+	size_t n = 0;
+
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return (0);
+}
