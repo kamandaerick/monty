f9187989e2a2a0c4fe7b6d354827974ab8451e44
Erick Kamanda <erickckamanda@gmail.com>
2023-08-24 00:33:45 -0700

    Handled memory allocation error message

---
+++ ma_error.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * ma_error - prints appropriate message when memory allocation fails
+ */
+
+void ma_error()
+{
+	fprintf(stderr, "Error: malloc failed\n");
+	free_args();
+	exit(EXIT_FAILURE);
+}
